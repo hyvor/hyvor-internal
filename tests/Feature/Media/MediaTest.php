58c4ed0efd45b5da6cbbc364f87d91c935b2fec8
SupunKavinda <supunkavinda1125@gmail.com>
2024-02-24 17:43:40 +0100

    media Wip

---
+++ tests/Feature/Media/MediaTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Hyvor\Helper\Tests\Feature\Media;
+
+use Illuminate\Support\Facades\Storage;
+
+it('serves media', function() {
+
+    Storage::put(
+        'files/test.txt',
+        'test-contents'
+    );
+
+    $response = $this->get('/api/media/files/test.txt')
+        ->assertOk()
+        ->assertHeader('Cache-Control', 'max-age=31536000, public')
+        ->assertHeader('ETag');
+
+    expect($response->streamedContent())->toBe('test-contents');
+
+});
