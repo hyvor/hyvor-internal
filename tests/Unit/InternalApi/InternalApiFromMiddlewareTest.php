a5823fc790d3d4a63370e35e7a1692bdccd95d3b
SupunKavinda <supunkavinda1125@gmail.com>
2024-08-05 14:00:37 +0200

    test fix

--- tests/Unit/InternalApi/InternalApiFromMiddlewareTest.php
+++ tests/Unit/InternalApi/InternalApiFromMiddlewareTest.php
@@ -2,8 +2,18 @@
 
 namespace Hyvor\Internal\Tests\Unit\InternalApi;
 
+it('does not allow missing component', function () {
+    test()
+        ->get(
+            '/api/internal/internal-api-testing-test-route-from-middleware'
+        )
+        ->assertStatus(403)
+        ->assertSee('Missing from component');
+});
+
 it('does not allow wrong from component', function () {
     test()
+        ->withHeader('X-Internal-Api-From', 'talk')
         ->get(
             '/api/internal/internal-api-testing-test-route-from-middleware'
         )
