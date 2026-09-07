c825e02edd24d59021b39321e531b7728dce1297
Supun Wimalasena <supun@hyvor.com>
2026-09-07 17:57:28 +0200

    fix: MetricMiddleware route null

--- src/Metric/MetricMiddleware.php
+++ src/Metric/MetricMiddleware.php
@@ -65,7 +65,7 @@ class MetricMiddleware
          * This returns placeholder /api/sudo/user/{id} for /api/sudo/user/123
          */
         $route = $request->route();
-        return '/' . $route->uri();
+        return $route instanceof Route ? $route->uri() : $request->path();
     }
 
     private function getMetricsResponse(): mixed
