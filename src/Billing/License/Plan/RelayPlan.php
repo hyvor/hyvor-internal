a4f8090f15550041d48d50b4de2a798abf5fca97
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2026-09-10 23:30:02 +0530

    plan

--- src/Billing/License/Plan/RelayPlan.php
+++ src/Billing/License/Plan/RelayPlan.php
@@ -2,14 +2,41 @@
 
 namespace Hyvor\Internal\Billing\License\Plan;
 
+use Hyvor\Internal\Billing\License\RelayLicense;
+
+/**
+ * @extends PlanAbstract<RelayLicense>
+ */
 class RelayPlan extends PlanAbstract
 {
-    /**
-     * @codeCoverageIgnore
-     */
     protected function config(): void
     {
         $this->version(1, function () {
+            $this->plan(
+                'starter',
+                30,
+                new RelayLicense(
+                    emails:200_000,
+                    projects: 3,
+                    domains: 10,
+                    dedicatedIps: false
+                ),
+                nameReadable: 'Starter',
+                monthlyOnly: true
+            );
+
+            $this->plan(
+                'pro',
+                80,
+                new RelayLicense(
+                    emails: 800_000,
+                    projects: 20,
+                    domains: 1_000,
+                    dedicatedIps: true
+                ),
+                nameReadable: 'Pro',
+                monthlyOnly: true
+            );
         });
     }
 }
